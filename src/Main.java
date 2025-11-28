e528168dea56c403d9b9250ff64fc1611cc53966
rinat-ast <rinat-90210@mail.ru>
2025-11-28 21:47:01 +0400

    initial hw-array-for-check2

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,51 @@ public class Main {
         float[] fl = new float[]{1.57f, 7.654f, 9.986f};
         byte[] bt = new byte[]{2, 3, 5};
         System.out.println("Задача № 2 ");
-        System.out.println(Arrays.toString(num));
-        System.out.println(Arrays.toString(fl));
-        System.out.println(Arrays.toString(bt));
+        for (int i = 1; i <= num.length; i++) {
+            if (i == num.length) {
+                System.out.println(num[i - 1]);
+            } else {
+                System.out.print(num[i - 1] + ", ");
+            }
+        }
+        for (int i = 1; i <= fl.length; i++) {
+            if (i == fl.length) {
+                System.out.println(fl[i - 1]);
+            } else {
+                System.out.print(fl[i - 1] + ", ");
+            }
+        }
+        for (int i = 1; i <= bt.length; i++) {
+            if (i == bt.length) {
+                System.out.println(bt[i - 1]);
+            } else {
+                System.out.print(bt[i - 1] + ", ");
+            }
+        }
         System.out.println("Задача № 3");
         System.out.println("-----");
         for (int i = num.length; i > 0; i--) {
-            System.out.println(num[i - 1]);
-
+            if (i == 1) {
+                System.out.println(num[i - 1]);
+            } else {
+                System.out.print(num[i - 1] + ", ");
+            }
         }
         System.out.println("-----");
-        for (int i1 = fl.length; i1 > 0; i1--) {
-            System.out.println(fl[i1 - 1]);
+        for (int i = fl.length; i > 0; i--) {
+            if (i == 1) {
+                System.out.println(fl[i - 1]);
+            } else {
+                System.out.print(fl[i - 1] + ", ");
+            }
         }
         System.out.println("-----");
-        for (int i2 = bt.length; i2 > 0; i2--) {
-            System.out.println(bt[i2 - 1]);
+        for (int i = bt.length; i > 0; i--) {
+            if (i == 1) {
+                System.out.println(bt[i - 1]);
+            } else {
+                System.out.print(bt[i - 1] + ", ");
+            }
         }
         System.out.println("-----");
         System.out.println("Задача № 4");
@@ -35,6 +64,13 @@ public class Main {
             System.out.println(num[i]);
 
         }
+        String[] word = new String[]{"Mama", "Papa", "sister", "brother"};
+        for (int i = word.length; i > 0; i--) {
+            System.out.print(word[i - 1] + " ");
+
+        }
+        System.out.println(word[1]);
+        System.out.println(Arrays.toString(word));
 
     }
 }
