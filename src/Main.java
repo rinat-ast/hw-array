c48ea10240b8a792f39f871af0d64d61c516d4d5
rinat-ast <rinat-90210@mail.ru>
2025-11-28 14:23:33 +0400

    initial hw-array-for-check

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Задача № 1 ");
+        int[] num = new int[]{1, 2, 3};
+        float[] fl = new float[]{1.57f, 7.654f, 9.986f};
+        byte[] bt = new byte[]{2, 3, 5};
+        System.out.println("Задача № 2 ");
+        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(fl));
+        System.out.println(Arrays.toString(bt));
+        System.out.println("Задача № 3");
+        System.out.println("-----");
+        for (int i = num.length; i > 0; i--) {
+            System.out.println(num[i - 1]);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+        System.out.println("-----");
+        for (int i1 = fl.length; i1 > 0; i1--) {
+            System.out.println(fl[i1 - 1]);
+        }
+        System.out.println("-----");
+        for (int i2 = bt.length; i2 > 0; i2--) {
+            System.out.println(bt[i2 - 1]);
+        }
+        System.out.println("-----");
+        System.out.println("Задача № 4");
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] % 2 != 0) {
+                num[i]++;
+            }
+            System.out.println(num[i]);
+
+        }
+
     }
 }
+
